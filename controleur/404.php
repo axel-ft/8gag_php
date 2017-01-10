6d8579144bd36b8275e2b3caa3a67b8770e32a8b
axel-ft <axel.floquettrillot@ynov.com>
2017-01-10 21:58:51 +0000

    Ajout Page 404

---
+++ controleur/404.php
@@ -0,0 +1,2 @@
+<?php
+	require_once('vue/404.php');
---
+++ vue/404.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+	<title>404 - Non trouvé</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="vue/css/style.css">
+</head>
+<body>
+	<div id="p404">
+		<div id="t404">
+			<h1>404 - Page introuvable</h1>
+			<h3>Oups ! la page que vous avez demandé est introuvable&hellip;</h3>
+			<a href="index.php" class="bouton">Retour à l'accueil</a>
+		</div>
+	</div>
+</body>
+</html>
