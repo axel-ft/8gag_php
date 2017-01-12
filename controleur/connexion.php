742aa0919b726cbd2b85d67cce47f9b55f328f16
DavSCo <david.cohen@ynov.com>
2017-01-13 00:06:43 +0100

    MAJ Connexion pour user désactiver

--- controleur/connexion.php
+++ controleur/connexion.php
@@ -5,12 +5,15 @@ require_once ('modele/utilisateursql.php');
 $utilisateur = new UtilisateurSQL();
 if (!empty($_POST['mail']) && !empty($_POST['password'])) {
     $compte = $utilisateur->connect_compte();
-    if (count($compte)>0){
+    if (count($compte)>0 && intval($compte[0]['active']) === 1){
         $_SESSION['connected'] = true;
         $_SESSION['id_user'] = $compte[0]['id'];
         header('Location: index.php?p=mon_compte');
 
-    }else{
+    } elseif (intval($compte[0]['active']) === 0) {
+        $erreur = '<p>Compte désactivé</p>';
+    }
+    else{
         $erreur = '<p>Identifiant incorrect</p>';
     }
 
